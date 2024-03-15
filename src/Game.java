79da918867e5a85b334c261698b267e481eede93
marti <martinproks06@seznam.cz>
2024-03-15 13:11:58 +0100

    Added way to win the game

--- src/Game.java
+++ src/Game.java
@@ -6,4 +6,34 @@ public class Game {
 
 
 
+    public String win(){
+        if (pole.get(0).equals(pole.get(1))||pole.get(0).equals(pole.get(2))){
+            return pole.get(0) + " WON";
+        }
+        if (pole.get(3).equals(pole.get(4))||pole.get(3).equals(pole.get(5))){
+            return pole.get(0) + " WON";
+        }
+        if (pole.get(6).equals(pole.get(8))||pole.get(6).equals(pole.get(7))){
+            return pole.get(0) + " WON";
+        }
+        if (pole.get(0).equals(pole.get(3))||pole.get(0).equals(pole.get(6))){
+            return pole.get(0) + " WON";
+        }
+        if (pole.get(1).equals(pole.get(4))||pole.get(1).equals(pole.get(7))){
+            return pole.get(0) + " WON";
+        }
+        if (pole.get(2).equals(pole.get(5))||pole.get(2).equals(pole.get(8))){
+            return pole.get(0) + " WON";
+        }
+        if (pole.get(0).equals(pole.get(5))||pole.get(0).equals(pole.get(8))){
+            return pole.get(0) + " WON";
+        }
+        if (pole.get(2).equals(pole.get(4))||pole.get(2).equals(pole.get(6))){
+            return pole.get(0) + " WON";
+        }
+        return "";
+    }
+
+
+
 }
